f0aea83549386ae1e271eee778dca47a14890132
lenartd <daniel.lenart@akademiasovy.sk>
2018-10-12 08:56:33 +0200

    Finished

--- collatz.c
+++ collatz.c
@@ -2,16 +2,40 @@
 
 int main(int argc, char const *argv[]) {
   int num = 1;
-  int count = 0; //Use only for task 4.1, 4.2
-
-  /*Read input from user into variable num*/
-
-  /*Input validation*/
-
-  while (num != 1) {  //main cycle
-    /* code */
-    
-    
+  int count = 0;
+  int temp = 100;
+  int max = 0;
+  
+  while(temp != 200)
+  {
+  	temp++;
+  	num = temp;
+  	while (num != 1) 
+    {
+    	if(num%2 == 0)
+		{
+			num = num/2;	
+		}
+		else
+		{
+			num = (num*3)+1;
+		}
+		if(max < num)
+		{
+			max = num;
+		}
+		printf("%d\n", num);
+		count++;
+    }
+    printf("\n");
   }
+    printf("Count of main cycle operations: %d\n", count);
+    printf("The biggest number from the main cycle is: %d\n", max);
+   
+  
+  
+  
+  
+  
   return 0;
 }
